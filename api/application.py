83d47413c972084e4cf9cc9ea7857f12ccb10d43
wusisis <122709217+wusisis@users.noreply.github.com>
2022-11-05 09:33:48 +0000

    1.1.22

--- api/application.py
+++ api/application.py
@@ -40,4 +40,47 @@ from api.api_schemas import (
     VersionCreateResponse,
     VersionInfoResponse,
     VersionListResponse,
-    VersionMetadat
+    VersionMetadata,
+)
+from blocks import AsyncInvoker
+from database import Database
+from exceptions import ApplicationNotFound, InteractionNotFound
+from model import Application, ApplicationVersion
+from resolver import Resolver
+
+router = InferringRouter()
+
+
+@cbv(router)
+class ApplicationView:
+    """
+    The router class for LinguFlow. Any new api should be added here.
+    """
+
+    def __init__(self):
+        env = Env()
+        env.read_env()
+        self.database = Database(create_engine(env.str("DATABASE_URL")))
+        self.invoker = AsyncInvoker(self.database)
+        self.resolver = Resolver()
+
+    def resolve_params(self, params: Dict[str, inspect.Parameter]) -> List[Parameter]:
+        """
+        Resolves the parameters of a function.
+
+        Args:
+            params (Dict[str, inspect.Parameter]): A dictionary
+                mapping parameter names to inspect.Parameter objects.
+
+        Returns:
+            List[Parameter]: A list of Parameter objects representing the resolved parameters.
+
+        Example:
+        ```
+        def foo(x: int, y: bool) -> bool:
+            ...
+
+        signature = inspect.signature(foo)
+        params = dict(signature.parameters)
+
+        parameters = self.resolve_params(par
