0eb62665e4748a88d1c72f0ac54f9e768279772a
wusisis <122709217+wusisis@users.noreply.github.com>
2022-12-16 12:33:16 +0000

    Remove unused vim plugins

--- blocks/text.py
+++ blocks/text.py
@@ -23,4 +23,26 @@ class ComposeDict(BaseBlock):
 
 
 @block(name="Text_split_to_List", kind="data process")
-class ListParser(
+class ListParser(BaseBlock):
+    """
+    ListParser split a str into str list.
+
+    Example:
+
+    ```
+    node = ListParser(delim=",")
+    result = node("a,b,c")
+    ```
+
+    The result will be `["a", "b", "c"]`
+    """
+
+    def __init__(self, delim: str, prefix="", suffix=""):
+        super(ListParser, self).__init__()
+        self.delim = delim
+        self.prefix = prefix
+        self.suffix = suffix
+
+    def __call__(self, text: str) -> list:
+        text = text.strip().lstrip(self.prefix).rstrip(self.suffix)
+        return [t.strip() for t in text.split(self.delim) if t.strip()]
---
+++ blocks/tools.py
@@ -0,0 +1,51 @@
+
+import functools
+import urllib
+
+import requests
+
+from observability import span
+from patterns import Secret
+from resolver import block
+
+from .base import BaseBlock
+
+
+@functools.lru_cache
+def search_google(search_engine_id: str, key: str, query: str) -> dict:
+    query = urllib.parse.quote(query)
+    url = f"https://www.googleapis.com/customsearch/v1?cx={search_engine_id}&key={key}&q={query}"
+    return requests.get(url).json()
+
+
+@block(name="Google_Search", kind="tools")
+class GoogleSearch(BaseBlock):
+    """
+    This block searches Google to find related snippets.
+
+    Args:
+        search_engine_id (str): The custom search engine ID.
+        key (str): Your API key.
+        top_k (int, optional): The number of top results to return. Defaults to 5.
+
+    Example:
+
+    ```
+    node = GoogleSearch("<custom search engine id>", "<your api key>")
+    result = node("what is tidb?")
+    ```
+
+    Returns:
+        list: A list of snippets related to the search query.
+    """
+
+    def __init__(self, search_engine_id: str, key: Secret, top_k=5):
+        self.search_engine_id = search_engine_id
+        self.key = key
+        self.top_k = top_k
+
+    @span(name="search google")
+    def __call__(self, text: str) -> list:
+        r = search_google(self.search_engine_id, self.key, text)
+        items = r.get("items", [])
+        return [item["snippet"] for item in items[: self.top_k]]
