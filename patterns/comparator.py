9d8717bd7410dd5136001041acfe458280acb10d
wusisis <122709217+wusisis@users.noreply.github.com>
2024-02-02 18:47:57 +0000

    Remove stale comment

--- patterns/comparator.py
+++ patterns/comparator.py
@@ -4,4 +4,52 @@ from resolver import pattern
 
 
 @pattern(name="Int_Comparator")
-class NumberCompar
+class NumberComparator(ABC):
+    """
+    An abstract class for comparing integers.
+
+    All classes inherited from this one should take an int and returns a bool.
+    """
+
+    def __init__(self): ...
+
+    @abstractmethod
+    def __call__(self, input: int) -> bool: ...
+
+
+@pattern(name="Int_Greater_Or_Equal_Than")
+class GreaterOrEqualThan(NumberComparator):
+    """
+    GreaterOrEqualThan checks if the input number is greater than or equal to the target value.
+    """
+
+    def __init__(self, value: int):
+        self.value = value
+
+    def __call__(self, input: int) -> bool:
+        return input >= self.value
+
+
+@pattern(name="Int_Less_Or_Equal_Than")
+class LessOrEqualThan(NumberComparator):
+    """
+    LessOrEqualThan checks if the input number is less than or equal to the target value.
+    """
+
+    def __init__(self, value: int):
+        self.value = value
+
+    def __call__(self, input: int) -> bool:
+        return input <= self.value
+
+
+@pattern(name="Int_Greater_Than")
+class GreaterThan(NumberComparator):
+    """
+    GreaterThan checks if the input number is greater than the target value.
+    """
+
+    def __init__(self, value: int):
+        self.value = value
+
+    def __call__(self, input: int) -> b
