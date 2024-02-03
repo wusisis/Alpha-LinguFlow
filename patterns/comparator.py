8b9c268483fea5abaa9242c8f951e9b905299ce3
wusisis <122709217+wusisis@users.noreply.github.com>
2024-02-03 09:56:27 +0000

    add codacy config file

--- patterns/comparator.py
+++ patterns/comparator.py
@@ -52,4 +52,57 @@ class GreaterThan(NumberComparator):
     def __init__(self, value: int):
         self.value = value
 
-    def __call__(self, input: int) -> b
+    def __call__(self, input: int) -> bool:
+        return input > self.value
+
+
+@pattern(name="Int_Less_Than")
+class LessThan(NumberComparator):
+    """
+    LessThan checks if the input number is less than the target value.
+    """
+
+    def __init__(self, value: int):
+        self.value = value
+
+    def __call__(self, input: int) -> bool:
+        return input < self.value
+
+
+@pattern(name="Int_Equal_To")
+class EqualWithNumber(NumberComparator):
+    """
+    EqualWithNumber checks if the input number is equal to the target value.
+    """
+
+    def __init__(self, value: int):
+        self.value = value
+
+    def __call__(self, input: int) -> bool:
+        return input == self.value
+
+
+@pattern(name="Text_Comparator")
+class TextComparator(ABC):
+    """
+    An abstract class for comparing texts.
+
+    All classes inherited from this one should take a text and returns a bool.
+    """
+
+    def __init__(self): ...
+
+    @abstractmethod
+    def __call__(self, input: str) -> bool: ...
+
+
+@pattern(name="Text_Equal_With")
+class TextEqual(TextComparator):
+    """
+    TextEqual checks if the input text is equal to the target value.
+    """
+
+    def __init__(self, value: str):
+        self.value = value
+
+    d
