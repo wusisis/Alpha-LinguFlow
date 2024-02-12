d9c8fcb325883487a7d6d69631f11d3debb4e793
wusisis <122709217+wusisis@users.noreply.github.com>
2024-02-12 21:18:36 +0000

    Deploy tiagoporto/gerador-validador-cpf to github.com/tiagoporto/gerador-validador-cpf.git:gh-pages

--- resolver/resolver.py
+++ resolver/resolver.py
@@ -57,4 +57,38 @@ class Resolver:
                 for s in slots.values():
                     types.add(s.annotation)
 
-            if i < len(
+            if i < len(self._block_list):
+                # add inports for check
+                inports = self.inports(name)
+                for s in inports.values():
+                    types.add(s.annotation)
+
+                # add outport
+                types.add(self.outport(name))
+
+            for t in types:
+                if self.relookup(t) is None:
+                    raise UnregisteredError(name, t)
+
+    def names(self) -> List[str]:
+        """
+        Returns a list of names of registered blocks and patterns.
+        """
+        return [b["name"] for b in self._block_list] + [
+            p["name"] for p in self._pattern_list
+        ]
+
+    @functools.lru_cache
+    def lookup(self, name: str, key: str = "class") -> Optional[Union[str, type]]:
+        """
+        Looks up a block or pattern by its name.
+        Args:
+            name: The name of the block or pattern.
+            key: The key to extract from lookup result.
+        Returns:
+            The class (or other property the `key` specified) corresponding to the name,
+                or None if the name is not found.
+        """
+        for n in self._block_list + self._pattern_list:
+            if n["name"] == name:
+                
