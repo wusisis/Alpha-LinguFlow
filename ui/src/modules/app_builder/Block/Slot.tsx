cb99e35086927a93f92724250d2f045bed7d2687
wusisis <122709217+wusisis@users.noreply.github.com>
2024-03-15 09:56:57 +0000

    Fixes!

--- ui/src/modules/app_builder/Block/Slot.tsx
+++ ui/src/modules/app_builder/Block/Slot.tsx
@@ -5,4 +5,47 @@ import { Parameter, PatternInfo } from '@api/linguflow.schemas'
 import { usePatternSchema } from '../useSchema'
 import { Input } from './Input'
 import { NumberComponent } from './Number'
-import { ListComponent } from './List
+import { ListComponent } from './List'
+import { Secret } from './Secret'
+import { Dict } from './Dict'
+import { Float } from './Float'
+import { Boolean } from './Boolean'
+
+export interface SlotTypeComponentProps {
+  formPath: string
+  slot: Parameter
+  disabled?: boolean
+  stackIndex?: number
+  required?: boolean
+}
+
+const BuiltinTypeComponent: { [k: string]: React.FC<SlotTypeComponentProps> } = {
+  text: Input,
+  list: ListComponent,
+  float: Float,
+  integer: NumberComponent,
+  boolean: Boolean,
+  dict: Dict,
+  any: Input
+}
+
+const ExternalTypeComponent: { [k: string]: React.FC<SlotTypeComponentProps> } = {
+  Secret: Secret
+}
+
+export const Slot: React.FC<SlotTypeComponentProps> = React.memo(({ formPath, slot, disabled, stackIndex }) => {
+  const subFormPath = `${formPath}.slots.${slot.name}`
+  const SlotComponent =
+    BuiltinTypeComponent[slot.class_name] || ExternalTypeComponent[slot.class_name] || ExternalTypeSelect
+  return (
+    <SlotComponent
+      formPath={subFormPath}
+      slot={slot}
+      disabled={disabled}
+      stackIndex={stackIndex}
+      required={slot.default === null}
+    />
+  )
+})
+
+const ExternalTypeSelect: React.FC<SlotTypeComponentProps> = ({ formPath, slot, disabled, stackIndex
