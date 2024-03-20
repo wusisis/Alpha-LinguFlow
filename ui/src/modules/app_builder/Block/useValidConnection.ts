d4319126de3ff9a7ce544935bbdb052ef2cb87eb
wusisis <122709217+wusisis@users.noreply.github.com>
2024-03-20 22:17:07 +0000

    Fix logging

--- ui/src/modules/app_builder/Block/useValidConnection.ts
+++ ui/src/modules/app_builder/Block/useValidConnection.ts
@@ -8,3 +8,31 @@ export const BLOCK_PORT_ID_NULL = '__null__'
 export const BOOLEAN_CLASS_NAME = 'boolean'
 
 export const useValidConnection = () => {
+  const getNodeType = useNodeType()
+  const { patternMap } = usePatternSchema()
+  return (connection: Connection) => {
+    const outportClassName = getNodeType(connection.source!)?.data?.schema?.outport
+    const inportClassName = getNodeType(connection.target!)?.data?.schema.inports.find(
+      (inp) => inp.name === connection.targetHandle
+    )?.class_name
+
+    const notSelfNode = connection.source !== connection.target
+    const isConditionalTarget = connection.targetHandle === BLOCK_PORT_ID_NULL
+    const isOutportBoolean = outportClassName === BOOLEAN_CLASS_NAME
+    const isConditionalConnection = isConditionalTarget && isOutportBoolean
+    const isAnyTarget = !isConditionalTarget && !inportClassName
+    const isSameClass = outportClassName === inportClassName
+    const isOutportClassASubclassOfInportClass =
+      !!inportClassName && patternMap[inportClassName]?.candidates?.includes(outportClassName)
+
+    return (
+      notSelfNode && (isConditionalConnection || isAnyTarget || isSameClass || isOutportClassASubclassOfInportClass)
+    )
+  }
+}
+
+export const useNodeType = () => {
+  const store = useStoreApi()
+  const useNodeTypeFn = useCallback(
+    (id: string) =>
+      
