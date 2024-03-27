47e27874392d50bb8ef3cab70bedfbee00a4819b
wusisis <122709217+wusisis@users.noreply.github.com>
2024-03-27 11:47:27 +0000

    Added English name for language and find_what_looking_for:

--- ui/src/modules/app_builder/Canvas/useHotKeyMenu.ts
+++ ui/src/modules/app_builder/Canvas/useHotKeyMenu.ts
@@ -21,4 +21,37 @@ export const useHotKeyMenu = (toolbarFocusedRef: React.RefObject<boolean>) => {
     menuStatus.current.mouseX = e.clientX
     menuStatus.current.mouseY = e.clientY
   }
-  const onNodeMouseEnter: (event: Re
+  const onNodeMouseEnter: (event: React.MouseEvent<Element, MouseEvent>) => void = () => {
+    menuStatus.current.inPane = false
+  }
+  const onNodeMouseLeave: (event: React.MouseEvent<Element, MouseEvent>) => void = () => {
+    menuStatus.current.inPane = true
+  }
+
+  const showHotKeyMenu = () => {
+    if (!menuStatus.current.inPane || toolbarFocusedRef.current) {
+      return
+    }
+    setMenuPosition([menuStatus.current.mouseX, menuStatus.current.mouseY])
+    setHotKeyMenuOpened(true)
+  }
+  const onPaneContextMenu: (event: React.MouseEvent<Element, MouseEvent>) => void = (e) => {
+    e.preventDefault()
+    showHotKeyMenu()
+  }
+  const onNodeContextMenu: (event: React.MouseEvent<Element, MouseEvent>) => void = (e) => {
+    e.preventDefault()
+  }
+  const onEdgeContextMenu: (event: React.MouseEvent<Element, MouseEvent>) => void = (e) => {
+    e.preventDefault()
+  }
+
+  useEffect(() => {
+    const showHotKeyMenuHandler = getHotkeyHandler([['Space', showHotKeyMenu]])
+    document.body.addEventListener('keyup', showHotKeyMenuHandler)
+    return () => document.body.removeEventListener('keyup', showHotKeyMenuHandler)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return {
+    hotKeyMe
