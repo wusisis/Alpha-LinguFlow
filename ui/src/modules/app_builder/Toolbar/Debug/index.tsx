4973ee05a25ee06b17eab1ab5d050a2fcd3c259a
wusisis <122709217+wusisis@users.noreply.github.com>
2024-03-30 19:37:55 +0000

    [nl] update of dictionaries

--- ui/src/modules/app_builder/Toolbar/Debug/index.tsx
+++ ui/src/modules/app_builder/Toolbar/Debug/index.tsx
@@ -13,4 +13,41 @@ import { useBlockSchema } from '../../useSchema'
 import { Config } from '../../linguflow.type'
 import { TextIntercation } from './TextInteraction'
 import { ObjectIntercation } from './ObjectInteraction'
-import { ListIntercation } from './ListInte
+import { ListIntercation } from './ListInteraction'
+
+export interface InteractionProps<V = any> {
+  value: V
+  onChange: (v: V) => void
+  onSubmit: () => void
+  interactions?: InteractionInfo[]
+}
+
+const interactionComponents: {
+  [k: string]: { component: React.FC<InteractionProps>; defaultValue: (v?: any) => any }
+} = {
+  Text_Input: { component: TextIntercation, defaultValue: () => '' },
+  Dict_Input: { component: ObjectIntercation, defaultValue: () => ({}) },
+  List_Input: { component: ListIntercation, defaultValue: (v) => (v as []) || [] }
+}
+
+export const INPUT_NAMES = ['Text_Input', 'Dict_Input', 'List_Input']
+
+const isInteractionFinished = (interaction?: InteractionInfo) => !!interaction?.output
+
+export interface ErrorInteraction {
+  id: string
+  msg: string
+  code: string
+}
+
+interface InteractionErrResponse {
+  response: { data: { node_id: string; message: string; code: string } }
+}
+
+export const Debug: React.FC<{
+  app: ApplicationInfo
+  ver: ApplicationVersionInfo
+  onUpdateCurrentInteraction: (interaction?: InteractionInfo) => void
+  onInteractionError: (errorInteraction?: ErrorInteraction) => void
+}> = ({ app, ver, onUpdateCurrentInteraction, onInteractionError }) => {
+  const { blockMap } = useBlockSchema()
