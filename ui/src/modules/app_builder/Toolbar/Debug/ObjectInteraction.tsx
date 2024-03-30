8e4056335123dae7896b47a710b79bb2558f5b20
wusisis <122709217+wusisis@users.noreply.github.com>
2024-03-30 09:59:33 +0000

    N

--- ui/src/modules/app_builder/Toolbar/Debug/ObjectInteraction.tsx
+++ ui/src/modules/app_builder/Toolbar/Debug/ObjectInteraction.tsx
@@ -6,4 +6,38 @@ import { useState } from 'react'
 import type { InteractionProps } from '.'
 
 export const ObjectIntercation: React.FC<InteractionProps<ApplicationRunInputAnyOf>> = (props) => {
-  const {
+  const { value, onChange } = props
+  const [showInput, setShowInput] = useState(false)
+
+  return (
+    <Group align="flex-start">
+      <Tooltip label={showInput ? 'Switch to JSON tree' : 'Switch to raw'}>
+        <ActionIcon variant="subtle" color="gray" aria-label="Switch" onClick={() => setShowInput((v) => !v)}>
+          <IconSwitchHorizontal style={{ width: '70%', height: '70%' }} stroke={1.5} />
+        </ActionIcon>
+      </Tooltip>
+      {showInput ? (
+        <ObjectInput {...props} />
+      ) : (
+        <ReactJson
+          name={null}
+          iconStyle="square"
+          enableClipboard={false}
+          src={value}
+          onAdd={(v) => onChange(v.updated_src as ApplicationRunInputAnyOf)}
+          onEdit={(v) => onChange(v.updated_src as ApplicationRunInputAnyOf)}
+          onDelete={(v) => onChange(v.updated_src as ApplicationRunInputAnyOf)}
+        />
+      )}
+    </Group>
+  )
+}
+
+const ObjectInput: React.FC<InteractionProps<ApplicationRunInputAnyOf>> = ({ value, onChange }) => {
+  const [inputValue, setInputValue] = useState(JSON.stringify(value))
+  const [error, setError] = useState<React.ReactNode>(false)
+
+  return (
+    <Textarea
+      w="90%"
+      aut
