330f1a1851f6e950b594a6e6076ed6eae7a4d0ea
wusisis <122709217+wusisis@users.noreply.github.com>
2024-04-08 12:12:26 +0000

    wakeup: add toggle for bluedroid_timer wakelock

--- ui/src/modules/app_builder/useMutateVersion.ts
+++ ui/src/modules/app_builder/useMutateVersion.ts
@@ -10,4 +10,40 @@ import { ApplicationVersionInfo } from '@api/linguflow.schemas'
 import { useEffect, useState } from 'react'
 import { useGetLinguFlowEdge } from './Block/useValidConnection'
 
-exp
+export const getCurrentDateTimeName = () => `v${dayjs().format('YYYY-MM-DD')}.${dayjs().unix()}`
+
+export const useCreateVersion = (version?: ApplicationVersionInfo) => {
+  const { appId, verId } = useParams()
+  const navigate = useNavigate()
+  const { getNodes } = useReactFlow()
+  const getLinguFlowEdge = useGetLinguFlowEdge()
+  const { getValues, resetField } = useFormContext()
+  const { mutateAsync: _createVersion, isLoading } = useCreateAppVersionApplicationsApplicationIdVersionsPost()
+
+  const [canSave, setCanSave] = useState(false)
+  const createVersion = async (force = false) => {
+    if ((!canSave || isLoading) && !force) {
+      return
+    }
+    setCanSave(false)
+
+    const { id } = await _createVersion({
+      applicationId: appId!,
+      data: {
+        parentId: verId,
+        name: getCurrentDateTimeName(),
+        configuration: {
+          nodes: Object.values(getValues()),
+          edges: getLinguFlowEdge()
+        },
+        metadata: {
+          ...version?.metadata,
+          ui: {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            nodes: getNodes().map(({ data, ...n }) => n)
+          }
+        }
+      }
+    })
+
+    Object.keys(getValues()
